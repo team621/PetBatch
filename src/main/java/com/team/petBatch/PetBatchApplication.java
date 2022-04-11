191f788ba4aafc60e569694390fe8fa21725c70a
team621 <team621@naver.com>
2022-04-11 23:11:27 +0900

    gradle project 생성하여 다시 업로드

---
+++ src/main/java/com/team/petBatch/PetBatchApplication.java
@@ -0,0 +1,13 @@
+package com.team.petBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetBatchApplication.class, args);
+	}
+
+}
